13d5dec00467c86087542fe8c014529dd0305e89
AxelSotelo <88851084+AxelSotelo@users.noreply.github.com>
2021-09-03 11:58:15 -0500

    Update exercise.py

--- assignments/06PromedioConDecision/src/exercise.py
+++ assignments/06PromedioConDecision/src/exercise.py
@@ -1,5 +1,19 @@
 def main():
-    #escribe tu código abajo de esta línea
-    pass
-if __name__=='__main__':
+  #escribe tu código abajo de esta línea
+    
+    suma=0
+    nm=0
+    n=0
+    while n >= 0:
+        n=float(input("Dame un numero positivo, dame uno negativo para terminar: "))
+        if n>=0:
+            suma=suma+n
+            nm=nm+1
+        else:
+            break
+        
+    prom=suma/nm
+    print("El promedio es ", prom)
+    
+if __name__ == '__main__':
     main()
