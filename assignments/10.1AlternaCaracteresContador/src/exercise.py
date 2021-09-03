61725a2a63b343c2dcec6fbf290652fe0c2b7110
AxelSotelo <88851084+AxelSotelo@users.noreply.github.com>
2021-09-03 12:37:47 -0500

    Update exercise.py

--- assignments/10.1AlternaCaracteresContador/src/exercise.py
+++ assignments/10.1AlternaCaracteresContador/src/exercise.py
@@ -1,9 +1,16 @@
 def main():
   #escribe tu código abajo de esta línea
-
+    
+    n=int(input())
+    for i in range(1, n+1, 1):
+        if i%2==0:
+            print(str(i)+"-%")   
+        else:
+            print(str(i)+"-#")
             
         
 if __name__ == '__main__':
     main()
+
 
 
