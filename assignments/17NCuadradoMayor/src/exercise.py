ccada38e009752a99e6d334e9001ecc7edecdd95
AxelSotelo <88851084+AxelSotelo@users.noreply.github.com>
2021-09-03 13:04:15 -0500

    Update exercise.py

--- assignments/17NCuadradoMayor/src/exercise.py
+++ assignments/17NCuadradoMayor/src/exercise.py
@@ -1,12 +1,12 @@
 def main():
     num = int(input("Escribe un numero : "))
     #escribe tu código abajo de esta línea
-    
     v=1
-    while v**2<num:
+    while v**2<=num:
         v=v+1
     print(v)
 
 if __name__ == '__main__':
     main()
+
 
