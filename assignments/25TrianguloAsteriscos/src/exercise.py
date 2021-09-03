c2cd6cddc7835c89cdff653e5e2808e1cc734e77
AxelSotelo <88851084+AxelSotelo@users.noreply.github.com>
2021-09-03 13:24:56 -0500

    Update exercise.py

--- assignments/25TrianguloAsteriscos/src/exercise.py
+++ assignments/25TrianguloAsteriscos/src/exercise.py
@@ -1,8 +1,11 @@
-
 def main():
-    #Escribe tu código debajo de esta línea
-    pass
-
-
-if __name__=='__main__':
+    height = int(input("Enter triangle height: "))
+    #escribe tu código abajo de esta línea
+    
+    for i in range(1,height+1):
+        s=height-i        
+        print(" "*s+"*"*i)    
+    
+if __name__ == '__main__':
     main()
+
